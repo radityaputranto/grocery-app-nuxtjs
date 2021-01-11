dabc63572d35f38b11138099dd6c5fea1b2c4659
raditya putranto <radityaputranto97@yahoo.co.id>
2021-01-11 13:48:08 +0700

    products page, search button

--- pages/book/index.vue
+++ pages/book/index.vue
@@ -1,206 +1,68 @@
 <template>
   <b-container fluid>
     <h1>List Books</h1>
-    <!-- User Interface controls -->
-    <b-row class="mb-3">
-        <b-col lg="2" md="4" class="my-1">
-            <b-form-group
-            label="Per page"
-            label-for="per-page-select"
-            label-cols-sm="6"
-            label-cols-md="6"
-            label-cols-lg="6"
-            label-align-sm="left"
-            label-size="sm"
-            class="mb-0"
-            >
-            <b-form-select
-                id="per-page-select"
-                v-model="perPage"
-                :options="pageOptions"
-                size="sm"
-            ></b-form-select>
-            </b-form-group>
-        </b-col>
 
-        <b-col lg="10" md="8" class="my-1">
-            <b-row class="justify-content-end">
-                <b-col md="5" class="align-right">
-                    <b-form-group
-                    label="Filter"
-                    label-for="filter-input"
-                    label-cols-sm="3"
-                    label-align-sm="right"
-                    label-size="sm"
-                    class="mb-0  search-table"
-                    >
-                    <b-input-group size="sm">
-                        <b-form-input
-                        id="filter-input"
-                        v-model="filter"
-                        type="search"
-                        col="4"
-                        placeholder="Type to Search"
-                        ></b-form-input>
+    <div class="row">
+      <div class="col-12">
+        <div>
+          <b-input-group
+            size="lg"
+            class="mb-3"
+          >
+            <b-form-input placeholder="search products.." v-model="search" @keyup="searchFood"></b-form-input>
+            <b-input-group-append>
+              <b-button size="sm" text="Search" variant="outline-success"
+                ><b-icon-search></b-icon-search> Search</b-button
+              >
+            </b-input-group-append>
+          </b-input-group>
+        </div>
+      </div>
+    </div>
 
-                        <!-- <b-input-group-append>
-                        <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
-                        </b-input-group-append> -->
-                    </b-input-group>
-            </b-form-group>
-                </b-col>
-            </b-row>
-        </b-col>
-
-      
-
-      
-    </b-row>
-
-    <!-- Main table element -->
-    <b-table
-      :items="items"
-      :fields="fields"
-      :current-page="currentPage"
-      :per-page="perPage"
-      :filter="filter"
-      :filter-included-fields="filterOn"
-      :sort-by.sync="sortBy"
-      :sort-desc.sync="sortDesc"
-      :sort-direction="sortDirection"
-      stacked="md"
-      show-empty
-      small
-      @filtered="onFiltered"
-    >
-      <template #cell(name)="row">
-        {{ row.value.first }} {{ row.value.last }}
-      </template>
-
-      <template #cell(actions)="row">
-        <b-button size="sm" @click="info(row.item, row.index, $event.target)" class="mr-1">
-          Info modal
-        </b-button>
-        <b-button size="sm" @click="row.toggleDetails">
-          {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
-        </b-button>
-      </template>
-
-      <template #row-details="row">
-        <b-card>
-          <ul>
-            <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
-          </ul>
-        </b-card>
-      </template>
-    </b-table>
-    <b-row class="justify-content-end">
-        <b-col sm="6" md="3" class="my-1">
-        <b-pagination
-          v-model="currentPage"
-          :total-rows="totalRows"
-          :per-page="perPage"
-          align="fill"
-          size="sm"
-          class="my-0"
-        ></b-pagination>
-      </b-col>
-    </b-row>
-    <!-- Info modal -->
-    <b-modal :id="infoModal.id" :title="infoModal.title" ok-only @hide="resetInfoModal">
-      <pre>{{ infoModal.content }}</pre>
-    </b-modal>
+    <div class="row">
+      <div
+        class="col-md-3 col-6 mb-3"
+        v-for="product in products"
+        :key="product.id"
+      >
+        <Cardproducts :product="product" />
+      </div>
+    </div>
   </b-container>
-</template>
+</template> 
 
 <script>
-  export default {
-    data() {
-      return {
-        items: [
-          { isActive: true, age: 40, name: { first: 'Dickerson', last: 'Macdonald' } },
-          { isActive: false, age: 21, name: { first: 'Larsen', last: 'Shaw' } },
-          {
-            isActive: false,
-            age: 9,
-            name: { first: 'Mini', last: 'Navarro' },
-            _rowVariant: 'success'
-          },
-          { isActive: false, age: 89, name: { first: 'Geneva', last: 'Wilson' } },
-          { isActive: true, age: 38, name: { first: 'Jami', last: 'Carney' } },
-          { isActive: false, age: 27, name: { first: 'Essie', last: 'Dunlap' } },
-          { isActive: true, age: 40, name: { first: 'Thor', last: 'Macdonald' } },
-          {
-            isActive: true,
-            age: 87,
-            name: { first: 'Larsen', last: 'Shaw' },
-            _cellVariants: { age: 'danger', isActive: 'warning' }
-          },
-          { isActive: false, age: 26, name: { first: 'Mitzi', last: 'Navarro' } },
-          { isActive: false, age: 22, name: { first: 'Genevieve', last: 'Wilson' } },
-          { isActive: true, age: 38, name: { first: 'John', last: 'Carney' } },
-          { isActive: false, age: 29, name: { first: 'Dick', last: 'Dunlap' } }
-        ],
-        fields: [
-          { key: 'name', label: 'Person full name', sortable: true, sortDirection: 'desc' },
-          { key: 'age', label: 'Person age', sortable: true, class: 'text-center' },
-          {
-            key: 'isActive',
-            label: 'Is Active',
-            formatter: (value, key, item) => {
-              return value ? 'Yes' : 'No'
-            },
-            sortable: true,
-            sortByFormatted: true,
-            filterByFormatted: true
-          },
-          { key: 'actions', label: 'Actions' }
-        ],
-        totalRows: 1,
-        currentPage: 1,
-        perPage: 5,
-        pageOptions: [5, 10, 15, { value: 100, text: "All" }],
-        sortBy: '',
-        sortDesc: false,
-        sortDirection: 'asc',
-        filter: null,
-        filterOn: [],
-        infoModal: {
-          id: 'info-modal',
-          title: '',
-          content: ''
-        }
-      }
+import axios from "axios";
+export default {
+  data() {
+    return {
+      products: [],
+      search :''
+    };
+  },
+  methods: {
+    setProdct(data) {
+      this.products = data;
     },
-    computed: {
-      sortOptions() {
-        // Create an options list from our fields
-        return this.fields
-          .filter(f => f.sortable)
-          .map(f => {
-            return { text: f.label, value: f.key }
-          })
-      }
-    },
-    mounted() {
-      // Set the initial number of items
-      this.totalRows = this.items.length
-    },
-    methods: {
-      info(item, index, button) {
-        this.infoModal.title = `Row index: ${index}`
-        this.infoModal.content = JSON.stringify(item, null, 2)
-        this.$root.$emit('bv::show::modal', this.infoModal.id, button)
-      },
-      resetInfoModal() {
-        this.infoModal.title = ''
-        this.infoModal.content = ''
-      },
-      onFiltered(filteredItems) {
-        // Trigger pagination to update the number of buttons/pages due to filtering
-        this.totalRows = filteredItems.length
-        this.currentPage = 1
-      }
+    searchFood(){
+        axios
+      .get("http://localhost:8001/products?q="+this.search)
+      .then((response) => this.setProdct(response.data))
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
     }
-  }
+  },
+  mounted() {
+    axios
+      .get("http://localhost:8001/products")
+      .then((response) => this.setProdct(response.data))
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  },
+};
 </script>
