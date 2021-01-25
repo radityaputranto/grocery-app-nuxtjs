e81820bc6b513b11bc303f56cd2a3af7260cc893
raditya putranto <radityaputranto97@yahoo.co.id>
2021-01-26 00:07:49 +0700

    install scss plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,43 +1,43 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Grocery',
+    title: "Grocery",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { hid: "title", name: "title", content: "Nuxt.js project" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/fav.ico' },
+      { rel: "icon", type: "image/x-icon", href: "/fav.ico" },
       {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap'
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap"
       }
     ]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-    '~/assets/css/main.css'
+    //'~/assets/css/main.css',
+    { src: "~/assets/scss/main.scss", lang: "scss" }
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
-    '@nuxtjs/toast'
-    
+    "bootstrap-vue/nuxt",
+    "@nuxtjs/toast"
   ],
   bootstrapVue: {
     icons: true
@@ -45,21 +45,58 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.client) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/
+        });
+
+        const vueLoader = config.module.rules.find(
+          ({ loader }) => loader === "vue-loader"
+        );
+        const {
+          options: { loaders }
+        } = vueLoader || { options: {} };
+        if (loaders) {
+          for (const loader of Object.values(loaders)) {
+            changeLoaderOptions(Array.isArray(loader) ? loader : [loader]);
+          }
+        }
+        config.module.rules.forEach(rule => changeLoaderOptions(rule.use));
+        // console.log(util.inspect(config.module.rules, { depth: 6 }))
+      }
+    }
   },
   router: {
-    linkExactActiveClass: 'dark-green-text' // tailwind class with custom color
+    linkExactActiveClass: "dark-green-text" // tailwind class with custom color
   },
   toast: {
-    position: 'top-right',
-    register: [ // Register custom toasts
+    position: "top-right",
+    register: [
+      // Register custom toasts
       {
-        name: 'my-error',
-        message: 'Oops...Something went wrong',
+        name: "my-error",
+        message: "Oops...Something went wrong",
         options: {
-          type: 'error'
+          type: "error"
         }
       }
     ]
-}
+  }
+};
 
+function changeLoaderOptions(loaders) {
+  if (loaders) {
+    for (const loader of loaders) {
+      if (loader.loader === "sass-loader") {
+        Object.assign(loader.options, {
+          includePaths: ["./assets"]
+          // data: '@import "_imports";'
+        });
+      }
+    }
+  }
 }
